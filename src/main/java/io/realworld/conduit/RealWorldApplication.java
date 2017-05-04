40f6fd02c2829095fa7f6284036c326e3cb77f10
Keven van Zuijlen <kevenvz1@gmail.com>
2017-05-04 19:40:33 +0200

    Added some configuration variables

--- src/main/java/io/realworld/conduit/RealWorldApplication.java
+++ src/main/java/io/realworld/conduit/RealWorldApplication.java
@@ -1,12 +1,21 @@
 package io.realworld.conduit;
 
 import io.dropwizard.Application;
+import io.dropwizard.db.PooledDataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Environment;
 
 public class RealWorldApplication extends Application<RealWorldConfiguration> {
     public static void main(String[] args) throws Exception {
         new RealWorldApplication().run(args);
     }
+
+    /*private final HibernateBundle<RealWorldConfiguration> hibernateBundle =
+            new HibernateBundle<RealWorldConfiguration>() {
+                public PooledDataSourceFactory getDataSourceFactory(RealWorldConfiguration realWorldConfiguration) {
+                    return null;
+                }
+            };*/
 
     public void run(RealWorldConfiguration realWorldConfiguration, Environment environment) {
 
--- src/main/java/io/realworld/conduit/RealWorldConfiguration.java
+++ src/main/java/io/realworld/conduit/RealWorldConfiguration.java
@@ -1,6 +1,24 @@
 package io.realworld.conduit;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public class RealWorldConfiguration extends Configuration {
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.database = dataSourceFactory;
+    }
 }
