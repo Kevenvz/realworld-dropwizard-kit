aed4e4fce0b0a2c5330d523dba5c226e00a5644e
Keven van Zuijlen <kevenvz1@gmail.com>
2017-05-08 20:41:40 +0200

    Added migrationbundle and hibernatebundle

--- src/main/java/io/realworld/conduit/RealWorldApplication.java
+++ src/main/java/io/realworld/conduit/RealWorldApplication.java
@@ -1,23 +1,49 @@
 package io.realworld.conduit;
 
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.db.PooledDataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.realworld.conduit.core.User;
+import io.realworld.conduit.db.UserDAO;
 
 public class RealWorldApplication extends Application<RealWorldConfiguration> {
     public static void main(String[] args) throws Exception {
         new RealWorldApplication().run(args);
     }
 
-    /*private final HibernateBundle<RealWorldConfiguration> hibernateBundle =
-            new HibernateBundle<RealWorldConfiguration>() {
-                public PooledDataSourceFactory getDataSourceFactory(RealWorldConfiguration realWorldConfiguration) {
-                    return null;
+    private final HibernateBundle<RealWorldConfiguration> hibernateBundle =
+            new HibernateBundle<RealWorldConfiguration>(User.class) {
+                public PooledDataSourceFactory getDataSourceFactory(RealWorldConfiguration configuration) {
+                    return configuration.getDataSourceFactory();
                 }
-            };*/
+            };
 
-    public void run(RealWorldConfiguration realWorldConfiguration, Environment environment) {
+    @Override
+    public void initialize(Bootstrap<RealWorldConfiguration> bootstrap) {
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(
+                        bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
 
+        bootstrap.addBundle(new MigrationsBundle<RealWorldConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(RealWorldConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        });
+        bootstrap.addBundle(hibernateBundle);
+    }
+
+    public void run(RealWorldConfiguration configuration, Environment environment) {
+        final UserDAO userDAO = new UserDAO(hibernateBundle.getSessionFactory());
     }
 }
